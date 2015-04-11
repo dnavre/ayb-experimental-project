4849ddebee4a186f96faa2671f3f4696cf6c37c3
simon-zohrabyan <simon.zohrabyan@gmail.com>
2015-04-11 21:18:27 +0400

    Category all functionality add, edit, delete

--- web/modules/admin/category_save.php
+++ web/modules/admin/category_save.php
@@ -1,8 +1,32 @@
 <?php
 
 global $db;
+$name=$_POST['category_name'];
+if(isset($_POST['category_visible'])) $visible = true;
+else $visible = false;
 
+if($_POST['cat_id'] == '')
+{
+    $sql = "INSERT INTO category(name, visible, create_date) VALUES (:name, :visible, now())";
 
+    $stmt = $db->prepare($sql);
+
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':visible', $visible);
+    $stmt->execute();
+}
+else{
+    $id = $_POST['cat_id'];
+    $sql = "UPDATE category SET name = :name, visible = :visible WHERE id = :cat_id";
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':visible', $visible);
+    $stmt->bindParam(':cat_id', $id);
+    $stmt->execute();
+}
+header("Location: ?module=admin&action=category");
 
 
 
