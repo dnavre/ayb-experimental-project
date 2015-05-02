52cc1446bb9596df871e7523340e6bf64612633b
christine77777 <christine_gevorgyan@aybschool.am>
2015-05-02 19:36:45 +0400

    link

--- web/modules/admin/souvenir.php
+++ web/modules/admin/souvenir.php
@@ -51,7 +51,6 @@ $stmt->execute();
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 $result = $stmt->fetchAll();
 
-
 $smarty->assign("souvenirs", $result);
 $smarty->assign("menu_item", "souvenir");
 $smarty->display("admin/souvenir.tpl");
