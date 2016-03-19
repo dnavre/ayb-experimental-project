b7cc6c684a785c556b341a54c5cebcc9db457105
karinehovhannisyan <karine_hovhannisyan@aybschool.am>
2016-03-19 14:45:31 +0400

    admin page correction

--- web/modules/admin/page.php
+++ web/modules/admin/page.php
@@ -1,14 +1,22 @@
 <?php
-
 global $smarty, $db;
 
 $stmt = $db->prepare("select pa.id, pa.title, pa.visible
         from page pa
         order by pa.title");
 $stmt->execute();
-
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 $result = $stmt->fetchAll();
+var_dump($result);
+if (count($result) != 3){
+        if($result[0]['title'] != 'About Us'){
+                $stmt1 = $db -> prepare("INSERT INTO page (title, create_date, content) VALUES ('About Us', now(), '')");
+        }
+        if($result[1]['title'] != 'Contacts'){
+                $stmt1 = $db -> prepare("INSERT INTO page (title, create_date, content) VALUES ('Contacts', now(), '')");
+        }
+}
+
 $smarty->assign("pages", $result);
 $smarty->assign("menu_item", "page");
 $smarty->display("admin/page.tpl");
